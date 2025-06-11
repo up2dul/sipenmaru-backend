67ce6e1c1462202bcd7455f0fba8c7315838c04e
abdul <up2dul@gmail.com>
2025-06-12 00:47:12 +0700

    feat(database): add email field to applicants and status field to results table

--- database.sql
+++ database.sql
@@ -34,6 +34,7 @@ CREATE TABLE
     id INT AUTO_INCREMENT PRIMARY KEY,
     applicant_id INT NOT NULL,
     full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     birth_date DATE NOT NULL,
     address VARCHAR(255) NOT NULL,
     gender ENUM ('MALE', 'FEMALE') NOT NULL,
@@ -51,6 +52,7 @@ CREATE TABLE
     amount DOUBLE NOT NULL,
     payment_method VARCHAR(100) NOT NULL,
     payment_proof_url VARCHAR(255),
+    note VARCHAR(255),
     status ENUM ('PENDING', 'COMPLETED', 'INVALID') DEFAULT 'PENDING',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (applicant_id) REFERENCES applicants (id)
@@ -62,6 +64,7 @@ CREATE TABLE
     id INT AUTO_INCREMENT PRIMARY KEY,
     applicant_id INT NOT NULL,
     result BOOLEAN,
+    status ENUM ('IN_PROGRESS', 'PASSED', 'FAILED') DEFAULT 'IN_PROGRESS',
     note VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (applicant_id) REFERENCES applicants (id)

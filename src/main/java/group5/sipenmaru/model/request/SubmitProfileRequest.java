30ae943df519b8158970ce0429a9cf0c3601411e
abdul <up2dul@gmail.com>
2025-06-13 04:15:19 +0700

    feat(model): enhance request models with validation and JSON property annotations
    - Added validation annotations to SubmitPaymentProofRequest and SubmitProfileRequest for required fields.
    - Updated property names in SubmitPaymentProofRequest and SubmitProfileRequest to use JSON property annotations for better API compatibility.
    - Corrected the JSON property mapping in ProfileResponse for consistency with the updated request models.

--- src/main/java/group5/sipenmaru/model/request/SubmitProfileRequest.java
+++ src/main/java/group5/sipenmaru/model/request/SubmitProfileRequest.java
@@ -2,13 +2,13 @@ package group5.sipenmaru.model.request;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import group5.sipenmaru.entity.enums.Gender;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,26 +20,32 @@ import lombok.NoArgsConstructor;
 @Builder
 public class SubmitProfileRequest {
     @NotBlank
+    @Size(min = 3, max = 100)
+    @JsonProperty("full_name")
     private String fullName;
 
     @NotBlank
-    @Email
+    @Email(message = "Invalid email address")
+    @JsonProperty("email")
     private String email;
 
-    @NotBlank
-    @Enumerated(EnumType.STRING)
+    @NotNull
+    @JsonProperty("gender")
     private Gender gender;
 
     @NotNull
-    @Past
+    @JsonProperty("birth_date")
     private Date birthDate;
 
     @NotBlank
+    @JsonProperty("selected_major")
     private String selectedMajor;
 
     @NotBlank
+    @JsonProperty("address")
     private String address;
 
     @NotBlank
+    @JsonProperty("diploma_file_url")
     private String diplomaFileUrl;
 } 

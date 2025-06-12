c84911e27894aa027ef761c1c04899d0de143515
up2dul <up2dul@gmail.com>
2025-06-12 21:54:00 +0700

    feat(model): format all properties to response to use snake_case

--- src/main/java/group5/sipenmaru/model/PaymentMethodResponse.java
+++ src/main/java/group5/sipenmaru/model/PaymentMethodResponse.java
@@ -2,6 +2,8 @@ package group5.sipenmaru.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,7 +24,10 @@ public class PaymentMethodResponse {
     @Builder
     public static class PaymentMethod {
         private String name;
+
+        @JsonProperty("virtual_account_number")
         private String virtualAccountNumber;
+
         private List<String> instructions;
     }
 } 
--- src/main/java/group5/sipenmaru/model/ProfileResponse.java
+++ src/main/java/group5/sipenmaru/model/ProfileResponse.java
@@ -2,6 +2,8 @@ package group5.sipenmaru.model;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import group5.sipenmaru.entity.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,17 +15,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class ProfileResponse {
+    @JsonProperty("full_name")
     private String fullName;
 
     private String email;
 
     private Gender gender;
 
+    @JsonProperty("full_name")
     private Date birthDate;
 
+    @JsonProperty("selected_major")
     private String selectedMajor;
 
     private String address;
 
+    @JsonProperty("diploma_file_url")
     private String diplomaFileUrl;
 } 
